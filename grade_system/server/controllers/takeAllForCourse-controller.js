6c1eb23dbe5988bdbba1a828c3f3755b99eb1bba
Aleksandr Ivanov <lex7.ivanov@yandex.ru>
2018-12-03 22:15:10 +0400

    working controller

--- grade_system/server/controllers/takeAllForCourse-controller.js
+++ grade_system/server/controllers/takeAllForCourse-controller.js
@@ -3,31 +3,26 @@ const Grade = require("../models/grade");
 const taskCourseRelation = require("../models/taskcourserelation");
 const Task = require("../models/task");
 
-var result = {
+
+
+exports.GetAllbyId = async (req, res) => {
+  const result = {
     users: [],
     tasks: [],
     grades: []
-  };
-
-exports.GetAllbyId = async (req, res) => {
+  }; 
   
   const relatedUsers = await User.find({ courseId: req.params.id });
   result.users = relatedUsers;
-
   const relatedCTR = await taskCourseRelation.find({ courseId: req.params.id });
+  
   const relatedCTRIds = relatedCTR.map(x => x.taskId);
-  relatedCTRIds.forEach(async taskId => {
-    const relatedTasks = await Task.find({ _id: taskId });
-    console.log(relatedTasks);
-    result.tasks.push(relatedTasks);
-  });
+  const relatedTasks = await Task.find({ _id: relatedCTRIds });
+  result.tasks.push(relatedTasks);
 
   const relatedUsersIds = relatedUsers.map(x => x.id);
-  relatedUsersIds.forEach(async id => {
-    const relatedGrades = await Grade.find({ userId: id });
-    console.log(relatedGrades);
-    result.grades.push(relatedGrades);        
-  });
+  const relatedGrades = await Grade.find({ userId: relatedUsersIds });
+  result.grades.push(relatedGrades);        
 
   res.status(200).json(result);
 };
