a4db0f809989a3db23b2d2f5eadcccf7aa2fe374
Aleksandr Ivanov <lex7.ivanov@yandex.ru>
2019-05-12 16:42:39 +0400

    rework for login-controller

--- grade_system/server/controllers/login-controller.js
+++ grade_system/server/controllers/login-controller.js
@@ -1,31 +1,25 @@
 const User = require("../models/user");
-const HttpError = require("error").HttpError;
-const async = require('async');
 
-exports.login = function(req, res){
-const email = req.body.email;
-const password = req.body.password;
+exports.login = async (req, res) => {
+    const email = req.body.email && req.body.email.toLowerCase();
+    const password = req.body.password;
 
-    async.waterfall([
-        function(callback){
-            User.findOne({email}, callback);        
-        },
-        function(user, callback){
-            if (user) {
-                if (user.checkPassword(password)){
-                    callback(null, user);
-                } else {
-                    next(new HttpError(403,"Incorrect password"));
-                }
-            } else {
-                new HttpError(403, "User not found"); 
-            }
-        }
-    ], function (err, user) {
-        if (err) return next(err);
+    const getSuccess = () => {
         req.session.user = user._id;
         res.send("Sucses!");
-    });
+    }
+
+    try {
+        const user = await User.findOne({ email }, callback);
+        user
+            ? user.checkPassword(password)
+                ? getSuccess()
+                : res.code(403).send("Incorrect password")
+            : res.code(403).send("User not found");
+        
+    } catch (err) {
+        res.code(500).json(err);
+    }
 };
 
 
