53594d7c2a9488f3b0f9db0734e966030d0e9691
“Voytenko <djsong-777@mail.ru>
2019-05-12 16:33:40 +0400

    hz

--- grade_system/server/controllers/login-controller.js
+++ grade_system/server/controllers/login-controller.js
@@ -1,12 +1,32 @@
 const User = require("../models/user");
+const HttpError = require("error").HttpError;
+const async = require('async');
 
-exports.login = (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    
-    User.findOne({email}) 
-    .then(user => {
-            User.checkPassword(password)? res.status(200).json(user) : res.send("Incorrect password!");
-        })
-    .catch(rej => res.status(400).send("User with this email not found!"));
+exports.login = function(req, res){
+const email = req.body.email;
+const password = req.body.password;
+
+    async.waterfall([
+        function(callback){
+            User.findOne({email}, callback);        
+        },
+        function(user, callback){
+            if (user) {
+                if (user.checkPassword(password)){
+                    callback(null, user);
+                } else {
+                    next(new HttpError(403,"Incorrect password"));
+                }
+            } else {
+                new HttpError(403, "User not found"); 
+            }
+        }
+    ], function (err, user) {
+        if (err) return next(err);
+        req.session.user = user._id;
+        res.send("Sucses!");
+    });
 };
+
+
+
